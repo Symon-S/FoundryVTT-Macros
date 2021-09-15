4f1a22180bf5e43eae687e398a1282af98ee4483
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2021-09-15 07:17:25 +0000

    Actions with potency template by Drental

---
+++ PF2e/Contributions by others/Actions with Potency.js
@@ -0,0 +1,14 @@
+/* By Drental */
+
+/* Macro can be used in combination with different actions to create potency effects for actions like trip, disarm, grapple, etc. This is a template and would need to be slightly modified to do the other actions*/
+
+/* mods */
+const modifiers = [
+  new game.pf2e.Modifier('+1 Oversized Guisarme', 1, 'item')
+];
+
+/* perform action with mods */
+game.pf2e.actions.trip({ 
+  modifiers: modifiers,
+  event: event 
+});
