86f2ef91d9d5f56ecf71493562ed8c6e56288b5a
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2021-09-15 07:18:35 +0000

    Update Actions with Potency.js

--- PF2e/Contributions by others/Actions with Potency.js
+++ PF2e/Contributions by others/Actions with Potency.js
@@ -2,7 +2,7 @@
 
 /* Macro can be used in combination with different actions to create potency effects for actions like trip, disarm, grapple, etc. This is a template and would need to be slightly modified to do the other actions*/
 
-/* mods */
+/* mods template*/
 const modifiers = [
   new game.pf2e.Modifier('+1 Oversized Guisarme', 1, 'item')
 ];
