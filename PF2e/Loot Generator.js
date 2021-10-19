176acbe645d031891c24c0c24a366ee34de565fa
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2021-10-19 20:21:06 +0000

    Update Loot Generator.js

--- PF2e/Loot Generator.js
+++ PF2e/Loot Generator.js
@@ -1,35 +1,38 @@
-if (!game.user.isGM) { ui.notifications.error("You are unable to use this macro!"); return; }
+//Limit Macro use to GM
+if (!game.user.isGM) { return ui.notifications.error("You are unable to use this macro!"); }
 
+//Populate items
 const item = game.packs.get('pf2e.equipment-srd');
 const items = await item.getDocuments();
-console.log(items);
 
+//Populate Spells
 const spellz = game.packs.get('pf2e.spells-srd');
 const spellS = await spellz.getDocuments();
-console.log(spellS);
 
+//Dialog Inputs
 const dialogs = [	
 	{ label : `What type of item?`, type: `select`, options: ["Treasures","Permanents","Consumables"]},
-	{ label : `Level?`, type: `number`, options: [0]},
-	{ label : `Center range value if treasure<br>(20% in either direction will be evaluated)`, type: `number`},
-        { label : `Denomination?`, type: `select`, options: ["Gold","Silver"]},
+	{ label : `Level? (Only Permanents and Consumables)`, type: `number`, options: [0]},
+	{ label : `Center range value if treasure (Only Treasures)<br>(50% in either direction will be evaluated)`, type: `number`},
+        { label : `Denomination? (Only Treasures)`, type: `select`, options: ["Gold","Silver"]},
 	{ label : `Quantity?`, type: `number`, options: [1]}
 ];
 
+//Run Dialog and gather Data
 const picks = await quickDialog({title: 'Loot Generator', data: dialogs});
-console.log(picks);
 
-if ( Noan(picks[4]) || picks[4] === 0) { return ui.notifications.error("A quantity of at least 1 is required!");}
+//Throw Error if quantity is below 1
+if ( Noan(picks[4]) || picks[4] < 1) { return ui.notifications.error("A quantity of at least 1 is required!");}
 
-let itemArray = [...Array(picks[4]).keys()];
-console.log(itemArray);
+//Pre-prep a counter array
+let itemArray = [...Array(Math.round(picks[4])).keys()];
 let randomItems = [];
 
+//Treasures
 if (picks[0] === "Treasures") {
 	const treasure = items.filter(t => t.type === "treasure");
-	console.log(treasure);
 	if ( Noan(picks[2]) ) { 
-		ui.notifications.info("No center range was inputted, random treasures selected");
+		ui.notifications.info("No center range was entered, random treasures selected");
 		itemArray.forEach( r => {
 			let random = Math.floor(Math.random() * treasure.length);
 			randomItems.push({name: treasure[random].name, id: treasure[random].id});
@@ -39,18 +42,17 @@ if (picks[0] === "Treasures") {
 			if (output === undefined) { output = `<p><a class="entity-link" draggable="true" data-pack="pf2e.equipment-srd" data-id="${r.id}">${r.name}</a></p>` }
 			else { output = output + `<p><a class="entity-link" draggable="true" data-pack="pf2e.equipment-srd" data-id="${r.id}">${r.name}</a></p>` }
 		});
-		console.log(output);   
                 return ChatMessage.create({flavor: `<strong>Random ${picks[0]}</strong><br>${output}`, speaker: ChatMessage.getSpeaker(), whisper:["Y3VAI2m6FxZptFGd"]});
 	}
 	if ( picks[2] < 1) { return ui.notifications.error("A value greater than 1 needs to be entered for range")}
 	else {
 		let denomination = "gp";
-		let value = picks[2];
+		let value = Math.round(picks[2]);
 		let treasures = [];
 		if (picks[3] === "Silver") { 
 			if (Math.round(picks[2]) < 10) { 
 				denomination = "sp"; 
-				value = picks[2];
+				value = Math.round(picks[2]);
 				if (Math.round(picks[2]) < 4 ) { value = 1 }
 				treasures = treasure.filter(f => f.data.data.denomination.value === denomination && Ranges(value).includes(f.data.data.value.value)); }
 			else { 
@@ -58,12 +60,10 @@ if (picks[0] === "Treasures") {
 				value = Math.round(picks[2]) / 10; 
 				treasures = treasure.filter(f => f.data.data.denomination.value === denomination && Ranges(value).includes(f.data.data.value.value) );
 				let temp = treasure.filter(f => f.data.data.denomination.value === "sp" && Ranges(Math.round(picks[2])).includes(f.data.data.value.value));
-				console.log(temp);
 				if ( temp.length  > 0 ) { temp.forEach( t => treasures.push(t));}
 			}
 		}
 		else { treasures = treasure.filter(f => f.data.data.denomination.value === denomination && Ranges(value).includes(f.data.data.value.value)); }
-		console.log(treasures);
 
 		if (treasures.length === 0) { return ui.notifications.warn(`There are no treasures within 20% of ${value}${denomination}`); }
 		
@@ -76,23 +76,19 @@ if (picks[0] === "Treasures") {
 			if (output === undefined) { output = `<p><a class="entity-link" draggable="true" data-pack="pf2e.equipment-srd" data-id="${r.id}">${r.name}</a></p>` }
 			else { output = output + `<p><a class="entity-link" draggable="true" data-pack="pf2e.equipment-srd" data-id="${r.id}">${r.name}</a></p>` }
 		});
-		console.log(output);   
                 ChatMessage.create({flavor: `<strong>Random ${picks[0]}</strong><br>${output}`, speaker: ChatMessage.getSpeaker(), whisper:["Y3VAI2m6FxZptFGd"]});
 	}
 }
 
-
+// Permanents
 if (picks[0] === "Permanents") {
 	if(Noan(picks[1])) { return ui.notifications.error("Level of at least 0 must be entered");}
 	const treasure = items.filter(t => t.type === "armor" || t.type === "weapon" || t.type === "equipment" || t.slug.search("magic-wand") > -1);
-	console.log(treasure);
 	const treasures = treasure.filter( l => l.level === picks[1] );
-	console.log(treasures);
 	itemArray.forEach( r => {
 		let random = Math.floor(Math.random() * treasures.length);
 		randomItems.push({name: treasures[random].name, id: treasures[random].id, slug:treasures[random].slug})
 	});
-	console.log(randomItems);
 	let output;
 	randomItems.forEach( r => {
 		let slug = r.slug;
@@ -100,7 +96,6 @@ if (picks[0] === "Permanents") {
 			if(slug.search("magic-wand") > -1){
 				const level = parseInt(r.slug.substr(11,1));
 				const spells = spellS.filter(l => l.level === level && !l.isFocusSpell && !l.isRitual && !l.isCantrip);
-				console.log(spells);
 				const randomSpell = spells[Math.floor(Math.random() * spells.length)];
 				output = `<p><a class="entity-link" draggable="true" data-pack="pf2e.spells-srd" data-id="${randomSpell.id}">${r.name} of ${randomSpell.name}</a></p>`
 			}
@@ -110,7 +105,6 @@ if (picks[0] === "Permanents") {
 			if(slug.search("magic-wand") > -1){
 				const level = parseInt(r.slug.substr(11,1));
 				const spells = spellS.filter(l => l.level === level && !l.isFocusSpell && !l.isRitual && !l.isCantrip);
-				console.log(spells);
 				const randomSpell = spells[Math.floor(Math.random() * spells.length)];
 				output = output + `<p><a class="entity-link" draggable="true" data-pack="pf2e.spells-srd" data-id="${randomSpell.id}">${r.name} of ${randomSpell.name}</a></p>`
 			}
@@ -118,21 +112,18 @@ if (picks[0] === "Permanents") {
 			else {output = output + `<p><a class="entity-link" draggable="true" data-pack="pf2e.equipment-srd" data-id="${r.id}">${r.name}</a></p>` }
 		}
 	});
-	console.log(output);   
         ChatMessage.create({flavor: `<strong>Random ${picks[0]}</strong><br>${output}`, speaker: ChatMessage.getSpeaker(), whisper:["Y3VAI2m6FxZptFGd"]});
 }
 
+//Consumbales
 if (picks[0] === "Consumables") {
 	if(Noan(picks[1])) { return ui.notifications.error("Level of at least 0 must be entered");}
 	const treasure = items.filter(t => t.type === "consumable" && t.slug.search("magic-wand") === -1);
-	console.log(treasure);
 	const treasures = treasure.filter( l => l.level === picks[1] );
-	console.log(treasures);
 	itemArray.forEach( r => {
 		let random = Math.floor(Math.random() * treasures.length);
 		randomItems.push({name: treasures[random].name, id: treasures[random].id, slug:treasures[random].slug})
 	});
-	console.log(randomItems);
 	let output;
 	randomItems.forEach( r => {
 		let slug = r.slug;
@@ -140,7 +131,6 @@ if (picks[0] === "Consumables") {
 			if(slug.search("scroll-of-") > -1){
 				const level = parseInt(r.slug.substr(10,1));
 				const spells = spellS.filter(l => l.level === level && !l.isFocusSpell && !l.isRitual && !l.isCantrip);
-				console.log(spells);
 				const randomSpell = spells[Math.floor(Math.random() * spells.length)];
 				output = `<p><a class="entity-link" draggable="true" data-pack="pf2e.spells-srd" data-id="${randomSpell.id}">${r.name} of ${randomSpell.name}</a></p>`
 			}
@@ -150,7 +140,6 @@ if (picks[0] === "Consumables") {
 			if(slug.search("scroll-of-") > -1){
 				const level = parseInt(r.slug.substr(10,1));
 				const spells = spellS.filter(l => l.level === level && !l.isFocusSpell && !l.isRitual && !l.isCantrip);
-				console.log(spells);
 				const randomSpell = spells[Math.floor(Math.random() * spells.length)];
 				output = output + `<p><a class="entity-link" draggable="true" data-pack="pf2e.spells-srd" data-id="${randomSpell.id}">${r.name} of ${randomSpell.name}</a></p>`
 			}
@@ -158,14 +147,13 @@ if (picks[0] === "Consumables") {
 			else {output = output + `<p><a class="entity-link" draggable="true" data-pack="pf2e.equipment-srd" data-id="${r.id}">${r.name}</a></p>` }
 		}
 	});
-	console.log(output);   
         ChatMessage.create({flavor: `<strong>Random ${picks[0]}</strong><br>${output}`, speaker: ChatMessage.getSpeaker(), whisper:["Y3VAI2m6FxZptFGd"]});
 
 }
 
 function Ranges(x) {
-	const lowEnd = Math.round(x * 0.8);
-	const highEnd = Math.round(x * 1.2);
+	const lowEnd = Math.round(x * 0.5);
+	const highEnd = Math.round(x * 1.5);
 	const range = [];
 	for (let i = lowEnd; i <= highEnd; i++) {
 		range.push(i);
