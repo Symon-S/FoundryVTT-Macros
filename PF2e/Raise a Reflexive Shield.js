3bef50a55259593096f8212b3deb17b176e30e85
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2021-12-07 00:56:18 +0000

    Update Raise a Reflexive Shield.js

--- PF2e/Raise a Reflexive Shield.js
+++ PF2e/Raise a Reflexive Shield.js
@@ -7,12 +7,13 @@ const ITEM_UUID = 'Compendium.pf2e.feat-effects.pwbFFD6NzDooobKo'; // Effect: Re
 const source = (await fromUuid(ITEM_UUID)).toObject();
 source.flags.core ??= {};
 source.flags.core.sourceId = ITEM_UUID;
+console.log(source);
 for await (const actor of actors) {
     const existing = actor.itemTypes.effect.find((effect) => effect.getFlag('core', 'sourceId') === ITEM_UUID);
     if (existing) {
         await existing.delete();
     } else {
-                if (token.actor.itemTypes.armor.filter(s => s.isEquipped === true && s.data.data.armorType.value === "shield" && s.data.data.hp.value > s.data.data.brokenThreshold.value).length != 0) {
+                if (token.actor.itemTypes.armor.filter(s => s.isEquipped === true && s.data.data.category === "shield" && s.data.data.hp.value > s.data.data.brokenThreshold.value).length != 0) {
         await actor.createEmbeddedDocuments('Item', [source]);
         }
     }
