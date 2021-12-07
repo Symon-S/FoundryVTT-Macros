b2ed61ae59e7d587645383854cc11f0c5d2bc25e
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2021-12-07 02:06:01 +0000

    Update Raise a Reflexive Shield.js

--- PF2e/Raise a Reflexive Shield.js
+++ PF2e/Raise a Reflexive Shield.js
@@ -1,20 +1,19 @@
-game.pf2e.actions.raiseAShield({ actors: [token?.actor ?? actor ?? game.user.character].filter((actor) => actor) })
 const actors = canvas.tokens.controlled.flatMap((token) => token.actor ?? []);
-if (!actors.length && game.user.character) {
-    actors.push(game.user.character);
-}
-const ITEM_UUID = 'Compendium.pf2e.feat-effects.pwbFFD6NzDooobKo'; // Effect: Reflexive Shield
+const ITEM_UUID = 'Compendium.pf2e.feat-effects.pwbFFD6NzDooobKo';
+ // Effect: Reflexive Shield
 const source = (await fromUuid(ITEM_UUID)).toObject();
 source.flags.core ??= {};
 source.flags.core.sourceId = ITEM_UUID;
+source.data.rules.push({"key":"FlatModifier","label":"Raised Shield","selector":"ac","type":"circumstance","value":2},{"domain":"all","key":"RollOption","option":"self:shield:raised"});
 console.log(source);
 for await (const actor of actors) {
-    const existing = actor.itemTypes.effect.find((effect) => effect.getFlag('core', 'sourceId') === ITEM_UUID);
+    const existing = token.actor.itemTypes.effect.find((effect) => effect.getFlag('core', 'sourceId') === ITEM_UUID);
     if (existing) {
         await existing.delete();
     } else {
                 if (token.actor.itemTypes.armor.filter(s => s.isEquipped === true && s.data.data.category === "shield" && s.data.data.hp.value > s.data.data.brokenThreshold.value).length != 0) {
         await actor.createEmbeddedDocuments('Item', [source]);
+
         }
     }
 }
