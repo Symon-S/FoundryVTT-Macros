aa6e6e056db3b848a2fa3693f3fabae404ac3043
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2022-01-16 01:26:51 +0000

    Update Modded BM immunity tracker.js

--- PF2e/Modded BM immunity tracker.js
+++ PF2e/Modded BM immunity tracker.js
@@ -1,4 +1,5 @@
 /* Author ArthurTrumpet Modded by Symon S.
+The original version of this macro is in the Contributed by others section of the git.
 Uses the built in Battle medicine immunity effect to track who has healed whom.
 GM selects the token being healed, and chooses the healer from the drop down.
 The effect icon won't appear on the token, as you can potentially be battle medicined several times, and it would just clutter up the token. 
