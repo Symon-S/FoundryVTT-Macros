83aa99c2b4e49603302ae167a46f6fef729e6e29
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2022-03-02 02:49:40 +0000

    Minor data change

--- PF2e/Spellsling.js
+++ PF2e/Spellsling.js
@@ -175,7 +175,8 @@ async function Spellsling()
       }
       if (critt === 'success' || critt === 'criticalSuccess') {
         if (spc.data.item.data.data.damage.value === '' || spc.data.item.data.data.damage.value === undefined || Object.entries(spc.data.item.data.data.damage.value).length === 0 || !spc.spell.chatData.isAttack){
-          spc.spell.spell.data.data.level.value = spc.lvl;
+          if (spc.spell.spell.data.data.heightenedLevel === undefined) { spc.spell.spell.data.data.heightenedLevel = {value: spc.lvl}; }
+          else {spc.spell.spell.data.data.heightenedLevel.value = spc.lvl;}
           spc.spell.spell.toMessage();
         }
         if (critt === 'criticalSuccess' && (game.settings.get("pf2e","critRule") === 'doubledice')) { spc.formula = ddice; } 
