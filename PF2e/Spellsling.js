3baf7522eca244f52cef91318c6bfb260ad76be4
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2022-03-02 06:53:26 +0000

    Filter out focus spells that aren't cantrips when no focus points are available

--- PF2e/Spellsling.js
+++ PF2e/Spellsling.js
@@ -34,6 +34,7 @@ async function Spellsling()
 					  if(spa === null) { return; }
 					  if(!spa.chatData.isAttack) { return; }
             if(spa.expended) { return; }
+            if(spellData.isFocusPool && !spa.spell.isCantrip && token.actor.data.data.resources.focus.value === 0){ return; }
             let level = `lv${sp.level}`
             const name = spa.spell.name;
             const spRD = spa.spell.getRollData({spellLvl: sp.level});
