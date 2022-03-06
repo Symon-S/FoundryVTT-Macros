c56a6bfb5a578fdab8536bbef1477c0f2d34a88f
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2022-03-06 10:52:36 +0000

    Smaller initial size and customizable dialog size.

--- PF2e/Casters Spellbook.js
+++ PF2e/Casters Spellbook.js
@@ -6,17 +6,12 @@ build, or a caster with multiple spellcasting entries.
 This macro will sort by the spellcasting entries, available spell levels,
 and finally the spells you have at those levels
 Clicking on the spell expends the slot/uses/focus point and
-posts the spell to the Chat Log.
-You can keep the dialog open and minimize it, or move it to a more convenient location
-to keep using it until you either refresh or want to close it.
-A token must be selected to use the macro.
+posts the spell to the Chat Log. 
 */
 
 if(canvas.tokens.controlled.length === 0) { return ui.notifications.warn("Please select a token"); }
 if (canvas.tokens.controlled.length > 1) { return ui.notifications.warn("Please select only 1 token"); }
 if (!token.actor.isSpellcaster) { return ui.notifications.warn(`${token.actor.name} is not a Spellcaster`); }
-
-let focCheck = false;
 
 const script = async function Spells(id){
   for (const token of canvas.tokens.controlled) {
@@ -56,12 +51,13 @@ const script = async function Spells(id){
     buttons = {};
     let levels = [...new Set(spells.map(l => l.lvl))];
     levels.forEach((index,value)=> {
+      console.log(index);
       if (index === 0) { index = 'Cantrip'}
       async function Filter(){
         if (index === 'Cantrip') { spells = spells.filter(c => c.spell.spell.isCantrip); }
         else{ spells = spells.filter(l => l.lvl === index); }
       }
-      buttons[value] = {label: index, callback: async () => { await Filter(); await Spell(); }}
+      buttons[value] = {label: index, callback: async () => { await Filter(); console.log(spells); await Spell(); }}
     });
     await Diag({title: "Spell Level?", buttons});
   }
@@ -69,13 +65,15 @@ const script = async function Spells(id){
   async function Spell() {
     buttons = {};
     spells.forEach((value,index) => {
+      console.log(value,index);
       if (value.lvl !== value.spell.spell.data.data.level.value && !value.spell.spell.data.isCantrip && !value.spell.spell.data.isFocusSpell) {
         if (value.spell.spell.data.data.heightenedLevel === undefined) { value.spell.spell.data.data.heightenedLevel = {value: value.lvl}; }
         else {value.spell.spell.data.data.heightenedLevel.value = value.lvl;}
       }
-      async function Cast() { value.spell.spell.toMessage(); spells = value; }
+      async function Cast() { value.spell.spell.toMessage(); spells = value; console.log(spells);}
       async function Consume(){
         const s_entry = token.actor.itemTypes.spellcastingEntry.find(e => e.id === spells.sEId);
+        console.log(s_entry);
         if (spells.type === 'spontaneous') {
           if ( spells.spell.spell.isCantrip ) { return; }
           let data = duplicate(s_entry.data);
@@ -130,22 +128,22 @@ let content = `
     background-color:#44c767;
   }
 </style>
-<div><strong>Choose a Spellcasting Entry:</strong></div><script>${script}</script>`;
+<div><strong>Choose a Spellcasting Entry:</strong></div><script>${script}
+</script>`;
 token.actor.itemTypes.spellcastingEntry.forEach((value,index) => {
   const test = value.getSpellData();
   if (test.isFocusPool && !test.levels.some(x => x.isCantrip) && token.actor.data.data.resources.focus.value === 0){ return; }
   content = content + `<button name="button${index}" class="psya-buttons ${index}" type="button" value="${value.name}" onclick="Spells('${value.id}')">${value.name}</button>`
 });  
+await new Promise(async (resolve) => {
+ await new Dialog({
+    title:"Caster's Spellbook",
+    content,
+    buttons:{ Close: { label: "Close" } },
+    }).render(true);
+    setTimeout(resolve,1);
+});
 
-await Diag1({title: "Caster's Spellbook", content});
-
-async function Diag1({title,content} = {}) {
-	await new Promise(() => {
-		new Dialog({
-			title,
-                        content,
-			buttons:{ Close: { label: "Close" } },
-      content,
-		}).render(true);
-	});
-}
+document.getElementsByClassName("app window-app dialog")[document.getElementsByClassName("app window-app dialog").length - 1].style.width = "200px";
+document.getElementsByClassName("app window-app dialog")[document.getElementsByClassName("app window-app dialog").length - 1].style.resize = "both";
+document.getElementsByClassName("app window-app dialog")[document.getElementsByClassName("app window-app dialog").length - 1].style.overflow = "auto";
