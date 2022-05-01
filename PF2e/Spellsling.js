7fea3a5ccce60d41a240b4c2dc6cf3c59026da8a
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2022-05-01 05:23:55 +0000

    Searing Light Automation

--- PF2e/Spellsling.js
+++ PF2e/Spellsling.js
@@ -92,7 +92,19 @@ async function Spellsling()
         const type = await quickDialog({data: {label:'Choose Damage Type:', type: 'select', options:["bludgeoning","piercing"]}, title: `Choose a damage type`});
         spc.formula = spc.formula + `[${type}]`;
       }
-      if (token.actor.itemTypes.feat.some(s => s.slug === 'dangerous-sorcery') && spc.slug !== 'magnetic-acceleration' && Object.entries(spc.data.item.data.data.damage.value).length !== 0 && !spc.data.item.isCantrip) {
+      if (spc.slug === 'searing-light'){
+        if ( token.actor.itemTypes.feat.some(s => s.slug === 'dangerous-sorcery') ) {
+          if (!game.user.targets.first().actor.traits.has('undead') && !game.user.targets.first().actor.traits.has('fiend')) { 
+            spc.formula = spc.formula + `[fire]`; 
+          }
+          else {
+            const type = await quickDialog({data: {label:'Choose Damage Type:', type: 'select', options:["fire","good"]}, title: `Choose a damage type`});
+          spc.formula = `${(spc.lvl-3)*2 + 5}d6[fire] + ${(spc.lvl-3)*2 + 5}d6[good] + ${spc.lvl}[${type}]`;
+        	}
+      	}
+			}
+
+      if (token.actor.itemTypes.feat.some(s => s.slug === 'dangerous-sorcery') && spc.slug !== 'magnetic-acceleration' && spc.slug !== 'searing-light' && Object.entries(spc.data.item.data.data.damage.value).length !== 0 && !spc.data.item.isCantrip) {
         spc.formula = spc.formula + `[${spc.data.item.data.data.damage.value[0].type.value}]`;
       }
       const fsplit = spc.formula.split(" ");
@@ -154,26 +166,30 @@ async function Spellsling()
 if (spc.data.item.data.data.damage.value !== '' || spc.data.item.data.data.damage.value !== undefined || Object.entries(spc.spell.chatData.damage.value).length > 0){ traits = traits + `,damaging-effect`; }
       let flavName = `${spc.data.item.name} cast at Lv${spc.lvl}`;
       if (spc.data.item.isCantrip) { flavName = `${spc.data.item.name} (Cantrip)`; }
-let flavor = `<strong>Spellsling</strong><br><a class="entity-link content-link" data-pack="pf2e.spells-srd" data-id="${spc.cId}"><strong>${flavName}</strong></a> (${dos})<div class="tags">${ttags}</div><hr>`;
-      if (spc.slug === 'acid-splash') { flavor = `<strong>Spellsling</strong><br><a class="entity-link content-link" data-pack="pf2e.spells-srd" data-id="${spc.cId}"><strong>${spc.data.item.name} (Cantrip)</strong></a> (${dos})<div class="tags">${ttags}</div>` }
-      if (spc.isSave) {
+let flavor = `<strong>Spellsling</strong><br>${TextEditor.enrichHTML(`@Compendium[pf2e.spells-srd.${spc.data.item.name}]{${flavName}}`)} (${dos})<div class="tags">${ttags}</div><hr>`;
+      if (spc.slug === 'acid-splash') { flavor = `<strong>Spellsling</strong><br>${TextEditor.enrichHTML(`@Compendium[pf2e.spells-srd.Acid Splash]{${flavName}}`)} (${dos})<div class="tags">${ttags}</div>` }
+      if (spc.isSave && spc.slug !== 'chromatic-ray') {
+        let basic = true;
+        if (spc.data.item.data.data.save.basic === '') { basic = false; }
         flavor = flavor + `<span data-pf2-check='${spc.data.item.data.data.save.value}' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits}' data-pf2-label='${spc.data.item.name} DC'><strong>DC ${spc.DC} </strong>${spc.data.item.data.data.save.basic} ${spc.data.item.data.data.save.value} save</span>`;
       }
 
+
       if(spc.slug === 'chromatic-ray' && (critt === 'success' || critt === 'criticalSuccess')) {
-        flavor = `<strong>Spellsling</strong><br><a class="entity-link content-link" data-pack="pf2e.spells-srd" data-id="${spc.cId}"><strong>${flavName}</strong></a> (${dos})<div class="tags">${ttags}`;
+        flavor = `<strong>Spellsling</strong><br>${TextEditor.enrichHTML(`@Compendium[pf2e.spells-srd.${spc.data.item.name}]{${flavName}}`)} (${dos})<div class="tags">${ttags}`;
         spc.formula = '';
         ddice = '';
         const chroma = [
           {d:`{30}[fire]`,f:`<span class="tag tooltipstered" data-trait="fire" data-description="PF2E.TraitDescriptionFire">Fire</span></div><hr><p class='compact-text'>1.<strong>Red</strong> (fire) The ray deals 30 fire damage to the target. Double on a Critical.</p>`,dd:`{60}[fire]`},
           {d:`{40}[acid]`,f:`<span class="tag tooltipstered" data-trait="acid" data-description="PF2E.TraitDescriptionAcid">Acid</span></div><hr><p class='compact-text'>2.<strong>Orange</strong> (acid) The ray deals 40 acid damage to the target. Double on a Critical.</p>`,dd:`{80}[acid]`},
           {d:`{50}[electricity]`,f:`<span class="tag tooltipstered" data-trait="electricity" data-description="PF2E.TraitDescriptionElectricity">Electricity</span></div><hr><p class='compact-text'>3.<strong>Yellow</strong> <br>(electricity) The ray deals 50 electricity damage to the target. Double on a Critical.</p>`,dd:`{100}[electricity]`},
-          {d:`{25}[poison]`,f:`<span class="tag tooltipstered" data-trait="poison" data-description="PF2E.TraitDescriptionPoison">Poison</span></div><hr><p class='compact-text'>4.<strong>Green</strong> (poison) The ray deals 25 poison damage to the target, double on a Critical, and the target must succeed at a <span data-pf2-check='fortitude' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits}' data-pf2-label='${spc.data.item.name} DC'><strong>DC ${spc.DC} </strong>Fortitude save</span> or be <a class="entity-link content-link" draggable="true" data-pack="pf2e.conditionitems" data-id="MIRkyAjyBeXivMa7"><strong>Enfeebled 1</strong></a> for 1 minute (<a class="entity-link content-link" draggable="true" data-pack="pf2e.conditionitems" data-id="MIRkyAjyBeXivMa7"><strong>Enfeebled 2</strong></a> on a critical failure).</p>`,dd:`{50}[poison]`},
-          {f:`</div><hr><p class='compact-text'>5.<strong>Blue</strong> The ray has the effect of the <a class="entity-link content-link" data-pack="pf2e.spells-srd" data-id="znv4ECL7ZtuiagtA"><strong>flesh to stone</strong></a> spell. On a critical hit, the target is <a class="entity-link content-link" draggable="true" data-pack="pf2e.conditionitems" data-id="i3OJZU2nk64Df3xm"><strong>Clumsy 1</strong></a> as long as it’s slowed by the flesh to stone effect.<br><span data-pf2-check='fortitude' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits}' data-pf2-label='Stone to Flesh DC'><strong>DC ${spc.DC} </strong>Fortitude save</span></p>`},
-          {f:`<span class="tag tooltipstered" data-trait="emotion" data-description="PF2E.TraitDescriptionEmotion">Emotion</span><span class="tag tooltipstered" data-trait="incapacitation" data-description="PF2E.TraitDescriptionIncapacitation">Incapacitation</span><span class="tag tooltipstered" data-trait="mental" data-description="PF2E.TraitDescriptionMental">Mental</span></div><hr><p class='compact-text'>6.<strong>Indigo</strong> (emotion, incapacitation, mental) The ray has the effect of the <a class="entity-link content-link" data-pack="pf2e.spells-srd" data-id="LiGbewa9pO0yjbsY"><strong>confusion</strong></a> spell. On a critical hit, it has the effect of <a class="entity-link content-link" data-pack="pf2e.spells-srd" data-id="8kJbiBEjMWG4VUjs"><strong>warp mind</strong></a> instead.<br><span data-pf2-check='will' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits},emotion,incapacitation,mental' data-pf2-label='Indigo DC'><strong>DC ${spc.DC} </strong>Will save</span></p>`},
-          {f:`</div><hr><p class='compact-text'>7.<strong>Violet</strong> <br>The target is <a class="entity-link content-link" draggable="true" data-pack="pf2e.conditionitems" data-id="xYTAsEpcJE1Ccni3"><strong>Slowed</strong></a> for 1 minute. It must also succeed at a <span data-pf2-check='will' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits}' data-pf2-label='Violet DC'><strong>DC ${spc.DC} </strong>Will save</span> or be teleported 120 feet directly away from you (if there isn’t room for it to appear there, it appears in the nearest open space); this is a teleportation effect.</p>`},
-          {f:`</div><hr><p class='compact-text'>8.<strong>Intense Color</strong> The target is <a class="entity-link content-link" draggable="true" data-pack="pf2e.conditionitems" data-id="TkIyaNPgTZFBCCuh"><strong>Dazzled</strong></a> until the end of your next turn, or <a class="entity-link content-link" draggable="true" data-pack="pf2e.conditionitems" data-id="XgEqL1kFApUbl5Z2"><strong>Blinded</strong></a> if your attack roll was a critical hit. Roll again and add the effects of another color (rerolling results of 8).</p>`},
+          {d:`{25}[poison]`,f:`<span class="tag tooltipstered" data-trait="poison" data-description="PF2E.TraitDescriptionPoison">Poison</span></div><hr><p class='compact-text'>4.<strong>Green</strong> (poison) The ray deals 25 poison damage to the target, double on a Critical, and the target must succeed at a <span data-pf2-check='fortitude' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits},poison' data-pf2-label='${spc.data.item.name} DC'><strong>DC ${spc.DC} </strong>Fortitude save</span> or be ${TextEditor.enrichHTML('@Compendium[pf2e.conditionitems.Enfeebled]{Enfeebled 1}')} for 1 minute (${TextEditor.enrichHTML('@Compendium[pf2e.conditionitems.Enfeebled]{Enfeebled 2}')} on a critical failure).</p>`,dd:`{50}[poison]`},
+          {f:`</div><hr><p class='compact-text'>5.<strong>Blue</strong> The ray has the effect of the ${TextEditor.enrichHTML(`@Compendium[pf2e.spells-srd.Flesh to Stone]{Flesh to Stone}`)} spell. On a critical hit, the target is ${TextEditor.enrichHTML('@Compendium[pf2e.conditionitems.Clumsy]{Clumsy 1}')} as long as it’s slowed by the flesh to stone effect.<br><span data-pf2-check='fortitude' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits}' data-pf2-label='${spc.data.item.name} DC'><strong>DC ${spc.DC} </strong>Fortitude save</span></p>`},
+          {f:`<span class="tag tooltipstered" data-trait="emotion" data-description="PF2E.TraitDescriptionEmotion">Emotion</span><span class="tag tooltipstered" data-trait="incapacitation" data-description="PF2E.TraitDescriptionIncapacitation">Incapacitation</span><span class="tag tooltipstered" data-trait="mental" data-description="PF2E.TraitDescriptionMental">Mental</span></div><hr><p class='compact-text'>6.<strong>Indigo</strong> (emotion, incapacitation, mental) The ray has the effect of the ${TextEditor.enrichHTML(`@Compendium[pf2e.spells-srd.Confusion]{Confusion}`)} spell. On a critical hit, it has the effect of ${TextEditor.enrichHTML(`@Compendium[pf2e.spells-srd.Warp Mind]{Warp Mind}`)} instead.<br><span data-pf2-check='will' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits},emotion,incapacitation,mental' data-pf2-label='Indigo DC'><strong>DC ${spc.DC} </strong>Will save</span></p>`},
+          {f:`</div><hr><p class='compact-text'>7.<strong>Violet</strong> <br>The target is ${TextEditor.enrichHTML('@Compendium[pf2e.conditionitems.Slowed]{Slowed}')} for 1 minute. It must also succeed at a <span data-pf2-check='will' data-pf2-dc='${spc.DC}' data-pf2-traits='${traits}' data-pf2-label='Violet DC'><strong>DC ${spc.DC} </strong>Will save</span> or be teleported 120 feet directly away from you (if there isn’t room for it to appear there, it appears in the nearest open space); this is a teleportation effect.</p>`},
+          {f:`</div><hr><p class='compact-text'>8.<strong>Intense Color</strong> The target is ${TextEditor.enrichHTML('@Compendium[pf2e.conditionitems.Dazzled]{Dazzled}')} until the end of your next turn, or ${TextEditor.enrichHTML('@Compendium[pf2e.conditionitems.Blinded]{Blinded}')} if your attack roll was a critical hit. Roll again and add the effects of another color (rerolling results of 8).</p>`},
         ];
+
         let chromaD = '1d4';
         if (spc.lvl > 5) { 
           chromaD = '1d8';
@@ -206,15 +222,15 @@ let flavor = `<strong>Spellsling</strong><br><a class="entity-link content-link"
 
 
       if(spc.slug === 'acid-splash' && critt === 'criticalSuccess') {
-        flavor = flavor + `<hr><a class="inline-roll roll persistent-link" title="{${pers}}[persistent,acid]" data-mode="roll" data-flavor="" data-formula="{${pers}}[persistent,acid]" draggable="true" data-value="${pers}" data-damage-type="acid" ondragstart="PF2EPersistentDamage._startDrag(event)">Persistent Damage [Acid ${pers}]</a>`
+        flavor = flavor + `<hr>${TextEditor.enrichHTML(`[[/r {${pers}}[persistent,acid]]] @Compendium[pf2e.conditionitems.Persistent Damage]{Persistent Acid Damage}`)}`
       }
       if(spc.slug === 'produce-flame' && critt === 'criticalSuccess') {
         pers = Math.ceil(actor.level / 2) + "d4";
-        flavor = flavor + `<br><a class="inline-roll roll persistent-link" title="{${pers}}[persistent,fire]" data-mode="roll" data-flavor="" data-formula="{${pers}}[persistent,fire]" draggable="true" data-value="${pers}" data-damage-type="fire" ondragstart="PF2EPersistentDamage._startDrag(event)">Persistent Damage [Fire ${pers}]</a>`
+        flavor = flavor + `<br>${TextEditor.enrichHTML(`[[/r {${pers}}[persistent,fire]]] @Compendium[pf2e.conditionitems.Persistent Damage]{Persistent Fire Damage}`)}`
       }
       if(spc.slug === 'gouging-claw' && critt === 'criticalSuccess') {
         pers = Math.ceil(actor.level / 2) + "d4";
-        flavor = flavor + `<br><a class="inline-roll roll persistent-link" title="{${pers}}[persistent,bleed]" data-mode="roll" data-flavor="" data-formula="{${pers}}[persistent,bleed]" draggable="true" data-value="${pers}" data-damage-type="bleed" ondragstart="PF2EPersistentDamage._startDrag(event)">Persistent Damage [Bleed ${pers}]</a>`
+        flavor = flavor + `<br>${TextEditor.enrichHTML(`[[/r {${pers}}[persistent,bleed]]] @Compendium[pf2e.conditionitems.Persistent Damage]{Persistent Bleed Damage}`)}`
       }
 
       if (game.modules.has('xdy-pf2e-workbench')) {
