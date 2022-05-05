8b8d9f5ea7778739c85b057d807531d41829fcec
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2022-05-05 05:22:04 +0000

    Added the ability to roll Wand of Smoldering Fireballs

--- PF2e/Use Scroll or Wand.js
+++ PF2e/Use Scroll or Wand.js
@@ -1,4 +1,6 @@
 //This macro allows you to send scrolls and wands directly to chat as if you had clicked on them in your character sheet.
+//Also rolls the equipment item Wand of Smoldering Fireballs.
+
 new Dialog({
   title: "Wand or Scroll?",
   buttons: {
@@ -15,6 +17,7 @@ new Dialog({
 }).render(true);
 
 let title;
+const sW = [{name:"wand-of-smoldering-fireballs-3rd-level-spell",level: 3},{name:"wand-of-smoldering-fireballs-5th-level-spell",level: 5},{name:"wand-of-smoldering-fireballs-7th-level-spell",level: 7},{name:"wand-of-smoldering-fireballs-9th-level-spell",level: 9},];
 
 async function Scroll() {  
  
@@ -39,11 +42,31 @@ async function Wand(){
   token.actor.itemTypes.consumable.forEach(s => {
    if (s.data.data.consumableType.value === "wand" && s.data.data.charges.value > 0) { wand.push(s); }
   });
+  
+  token.actor.itemTypes.equipment.forEach( e => {
+    if (sW.map(n => n.name).includes(e.slug)) { wand.push(e); }
+  });
 
   if (wand.length === 0) { return ui.notifications.warn("You do don't have wands to cast from.");}
   const choiceW = await choose(wand.map(n => n.data.name),`Pick a Wand`);
   const chosen = await wand.find(x => x.data.name === choiceW);
-  await chosen.toMessage();
+  if (sW.map(n => n.name).includes(chosen.slug)) {
+    const spells = await game.packs.get('pf2e.spells-srd');
+    const sFBall = await spells.getDocument("sxQZ6yqTn0czJxVd");
+    const source = await (await fromUuid("Compendium.pf2e.equipment-srd.fomEZZ4MxVVK3uVu")).toObject();
+    source.data.spell.data = sFBall.data;
+    source.data.spell.heightenedLevel = sW.find(n => n.name === chosen.slug).level;
+    source.data.slug = "temp-wosf-holder";
+    await token.actor.createEmbeddedDocuments('Item', [source]);
+    const sFB = token.actor.items.find(s => s.slug === "temp-wosf-holder");
+    console.log(chosen);
+    sFB.data.data.spell.data.name = chosen.name;
+    sFB.data.data.spell.data.data.description.value = chosen.description;
+    console.log(sFB);
+    await sFB.castEmbeddedSpell();
+    await sFB.delete();
+  }
+  else{ await chosen.toMessage(); }
 }
 /*
   Choose
@@ -57,6 +80,7 @@ async function choose(options = [], prompt = ``){
       let dialog_options = (options[0] instanceof Array)
         ? options.map(o => `<option value="${o[0]}">${o[1]}</option>`).join(``)
         : options.map(o => `<option value="${o}">${o}</option>`).join(``);
+      console.log(dialog_options);
       let content = `
       <table style="width=100%">
         <tr><th>${prompt}</th></tr>
