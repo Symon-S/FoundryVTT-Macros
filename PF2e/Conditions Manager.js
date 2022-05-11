7602813b74565f9c355721480265256e538f90b7
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2022-05-11 06:39:07 +0000

    Added persistent damage module integration.

--- PF2e/Conditions Manager.js
+++ PF2e/Conditions Manager.js
@@ -6,6 +6,7 @@ All conditions are toggled on and off by having the tokens selected that you wou
 name of the condition. If the condition can have a value, simply click the + to increase the value or the - to decrease.
 The Clear a condition button is used to clear a specific condition off of a group of selected tokens.
 This macro is loosely adapted from the Apply Conditions macro created by cepvep.
+This macro also integrates with the PF2e persistent damage module.
 CSS design by websterguy
 */
 
@@ -64,20 +65,23 @@ const nF = [
 ];
 const script1 = async function CToggle(c) {
   for(const token of canvas.tokens.controlled) {
+   if (token.actor === null) { ui.notifications.warn(`${token.name} does not have an actor or is broken`); continue; }
    if (token.actor.itemTypes.condition.some(p => p.slug === c && p.data.data.references.parent !== undefined)) { continue; }
-   await token.actor.toggleCondition(c);
+   if (token.actor !== null) { await token.actor.toggleCondition(c); }
   }
 };
 
 const script2 = async function ICon(c) {
   for(const token of canvas.tokens.controlled) {
-   await token.actor.increaseCondition(c);
+   if (token.actor === null) { ui.notifications.warn(`${token.name} does not have an actor or is broken`); continue; }
+   if (token.actor !== null) { await token.actor.increaseCondition(c); }
   }
 };
 
 const script3 = async function DCon(c) {
   for(const token of canvas.tokens.controlled) {
-   await token.actor.decreaseCondition(c);
+   if (token.actor === null) { ui.notifications.warn(`${token.name} does not have an actor or is broken`); continue; }
+   if (token.actor !== null) { await token.actor.decreaseCondition(c); }
   }
 };
 
@@ -85,36 +89,84 @@ const script4 = async function CCon() {
   async function Clear(html) {
     const c = html.find("#condition")[0].value;
     for(const token of canvas.tokens.controlled) {
+      if (token.actor === null) { ui.notifications.warn(`${token.name} does not have an actor or is broken`); continue; }
+      if ( token.actor.itemTypes.effect.some(f => f.name === c) ) { (await token.actor.itemTypes.effect.find(n => n.name === c)).delete(); continue; }
       if (!token.actor.itemTypes.condition.some(p => p.slug === c) || token.actor.itemTypes.condition.some(p => p.slug === c && p.data.data.references.parent !== undefined)) { continue; }
-      await token.actor.toggleCondition(c);
+      if (token.actor !== null) { await token.actor.toggleCondition(c); }
     }
   }
   const cons = []; 
   canvas.tokens.controlled.forEach( t=> {
     t.actor.itemTypes.condition.forEach ( c => {
       if (c.data.data.references.parent !== undefined) { return; }
-      cons.push(c.slug)
+      cons.push(c.slug);
     });
+    if (game.modules.has("pf2e-persistent-damage") && game.modules.get("pf2e-persistent-damage").active) {
+      t.actor.itemTypes.effect.forEach( e => {
+        if (e.slug.includes("persistent-damage")) { cons.push(e.name) }
+      });
+    }
   });
   const ccon = [...new Set(cons)].sort();
-  if (ccon.length === 0) { return }
-  let choices = '';
-  ccon.forEach( cc => { choices += `<option value="${cc}">${cc[0].toUpperCase() + cc.substring(1)}</option>`; });
-  new Dialog({
-    title: "Condition to cleared from tokens",
-    content: `<p>Choose condition to clear from selected tokens: <select id="condition">${choices}</select>`,
-    buttons: {
-      ok: {
-        label: "Ok",
-        callback: (html) => {
-          Clear(html);
+  if (ccon.length === 0) { return ui.notifications.info("No conditions to clear at this time.") }
+  if (ccon.length === 1) { 
+    for(const token of canvas.tokens.controlled) {
+      if(token.actor.hasCondition(ccon[0])) {
+        await token.actor.toggleCondition(ccon[0]); 
+      }
+    }
+  }
+  else{
+    let choices = '';
+    ccon.forEach( cc => { choices += `<option value="${cc}">${cc[0].toUpperCase() + cc.substring(1)}</option>`; });
+    new Dialog({
+      title: "Condition to clear from tokens",
+      content: `<p>Choose condition to clear from selected tokens: <select id="condition">${choices}</select>`,
+      buttons: {
+        ok: {
+          label: "Ok",
+          callback: (html) => {
+            Clear(html);
+          },
+        },
+        cancel: {
+          label: "Cancel",
         },
       },
-      cancel: {
-        label: "Cancel",
+      default: "cancel",
+    }).render(true);
+  }
+}
+const script5 = async function PerDamMod() {
+  let pd = false
+  canvas.tokens.controlled.forEach( q => {
+    if (q.actor === null) {  return ui.notifications.warn(`${q.name} does not have an actor or is broken`); }
+    if (q.actor.itemTypes.effect.length === 0) { return; }
+    if (q.actor.itemTypes.effect.some(e => e.slug.includes("persistent-damage"))) { pd = true; }
+  });
+
+  if (pd === true) { 
+    await new Dialog({
+      title:"Add or Clear?",
+      buttons:{
+        No: {
+	  label: "Add",
+	  callback: () => { PF2EPersistentDamage.showDialog(); }
+	},
+     	Yes: { 
+	  label: "Clear",
+	  callback: () => {
+       	    canvas.tokens.controlled.forEach( t => {
+              if (t.actor === null) { ui.notifications.warn(`${t.name} does not have an actor or is broken`); return; }
+              t.actor.itemTypes.effect.filter(ef => ef.slug.includes("persistent-damage")).forEach( p => {  p.delete(); });
+       	    }); 
+      	  },
+     	},
       },
-    },
-  }).render(true);
+      default: "No",
+      },{width:250}).render(true);
+  }
+  if (pd === false) { PF2EPersistentDamage.showDialog(); }
 }
 
 let content = `
@@ -126,7 +178,7 @@ let content = `
    background: url(../assets/sheet/background.webp);
   }
 
-  .cond-buttons-pd {
+  .cond-buttons-pd, .cond-buttons-pd:focus {
     margin: 1px auto;
     width: 70%;
     height: fit-content;
@@ -167,16 +219,23 @@ let content = `
     text-shadow: 0 0 2px #fff;
   }
 
-</style><script>${script1}${script2}${script3}${script4}</script><div class="cond-cont">`
+</style><script>${script1}${script2}${script3}${script4}${script5}</script><div class="cond-cont">`
 
 condition_list.forEach((c,i) => {
     if (wV.includes(c)) {
      content += `<div class="cond-butt-set"><button name="button${i}" class="cond-buttons ${i}" type="button" onclick="CToggle('${c}')">${c[0].toUpperCase() + c.substring(1)}</button><button name="button${i}+" class="cond-buttons cond-buttons-small ${i}+" type="button" onclick="ICon('${c}')">+</button><button name="button${i}-" class="cond-buttons cond-buttons-small ${i}-" type="button" onclick="DCon('${c}')">-</button></div> `;
     }
     else {
-     if ( c === "persistent-damage" ) { content += `<button name="button${i}" class="cond-buttons-pd" type="button" onclick="CToggle('${c}')">Persistent Damage</button> ` }
+     if ( c === "persistent-damage" ) { 
+      	if (game.modules.has("pf2e-persistent-damage") && game.modules.get("pf2e-persistent-damage").active) {
+	  content += `<button name="button${i}" class="cond-buttons-pd" type="button" onclick="PerDamMod()">Persistent Damage</button> `
+	}
+	else {
+       	  content += `<button name="button${i}" class="cond-buttons-pd" type="button" onclick="CToggle('${c}')">Persistent Damage</button> ` 
+	}
+     }
      else{
-     content += `<button name="button${i}" class="cond-buttons ${i}" type="button" onclick="CToggle('${c}')">${c[0].toUpperCase() + c.substring(1)}</button> `;
+       content += `<button name="button${i}" class="cond-buttons ${i}" type="button" onclick="CToggle('${c}')">${c[0].toUpperCase() + c.substring(1)}</button> `;
     }
   }
 });
