1032aeff9f7062e94584276dffab6e05ce6a311b
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2022-06-09 02:57:43 +0000

    Fixed heightening for certain types of spells

--- PF2e/Eldritch Shot.js
+++ PF2e/Eldritch Shot.js
@@ -261,9 +261,7 @@ async function Eldritch_shot()
       }
       if (critt === 'success' || critt === 'criticalSuccess') {
         if (spc.slug !== 'chromatic-ray' && ( spc.data.item.data.data.damage.value === '' || spc.data.item.data.data.damage.value === undefined || Object.entries(spc.spell.chatData.damage.value).length === 0 || !spc.spell.chatData.isAttack) ){
-          if (spc.spell.spell.data.data.heightenedLevel === undefined) { spc.spell.spell.data.data.heightenedLevel = {value: spc.lvl}; }
-          else {spc.spell.spell.data.data.heightenedLevel.value = spc.lvl;}
-          await spc.spell.spell.toMessage();
+          return await s_entry.cast(spc.spell.spell,{slot: spc.index,level: spc.lvl,message: true});
         }
         if (critt === 'criticalSuccess' && (game.settings.get("pf2e","critRule") === 'doubledice')) { spc.formula = ddice; } 
         if (critt === 'criticalSuccess' && (game.settings.get("pf2e","critRule") === 'doubledamage')) {  ui.notifications.info('Spell damage will need to be doubled when applied'); }
