ac8ba78b4bc65e54cc1bf5465dadf2b8a05784ca
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2023-01-09 08:28:00 +0000

    -Fixed to work with DSN and on Forge -Fixed to work with Workbench autoroll damage

--- PF2e/Double Slice.js
+++ PF2e/Double Slice.js
@@ -1,14 +1,12 @@
-if ( canvas.tokens.controlled.length !== 1 ) { return ui.notifications.info( "Please select 1 token" ) }
+if (canvas.tokens.controlled.length !== 1) { return ui.notifications.info("Please select 1 token") }
 
-if ( game.user.targets.first() === undefined ) { return ui.notifications.info( "Please target a token" )}
+if ( !token.actor.itemTypes.feat.some(f => f.slug === "double-slice") ) { return ui.notifications.warn(`${token.name} does not have the Double Slice feat!`) }
 
-if ( !token.actor.itemTypes.feat.some(f => f.slug === "double-slice") ) { return ui.notifications.warn( `${token.name} does not have the Double Slice feat!` ) }
+if ( token.actor.itemTypes.weapon.filter( h => h.isMelee && h.isHeld && h.hands === "1" && h.handsHeld === 1 ).length > 2 ) { return ui.notifications.info("To use the double slice macro, only 2 one-handed melee weapons can be equipped at a time.") }
 
-if ( token.actor.itemTypes.weapon.filter( h => h.isMelee && h.isHeld && h.hands === "1" && h.handsHeld === 1 ).length > 2 ) { return ui.notifications.info( "To use the double slice macro, only 2 one-handed melee weapons can be equipped at a time." ) }
+if ( token.actor.itemTypes.weapon.filter( h => h.isMelee && h.isHeld && h.hands === "1" && h.handsHeld === 1  ).length < 2 ) { return ui.notifications.info("Please equip/draw 2 one-handed melee weapons.") }
 
-if ( token.actor.itemTypes.weapon.filter( h => h.isMelee && h.isHeld && h.hands === "1" && h.handsHeld === 1  ).length < 2 ) { return ui.notifications.info( "Please equip/draw 2 one-handed melee weapons." ) }
-
-const DamageRoll = CONFIG.Dice.rolls.find(( (R) => R.name === "DamageRoll" ));
+const DamageRoll = CONFIG.Dice.rolls.find(((R) => R.name === "DamageRoll"));
 
 let weapons = token.actor.system.actions.filter( h => h.item?.isMelee && h.item?.isHeld && h.item?.hands === "1" && h.item?.handsHeld === 1 );
 
@@ -30,6 +28,20 @@ function SDOS(cm) {
 }
 
 let pd,sd;
+function PD(cm) {
+    if ( cm.user.id === game.userId && cm.isDamageRoll && cm.item === primary.item ) {
+        pd = cm.rolls[0];
+    }
+}
+
+function SD(cm) {
+    if ( cm.user.id === game.userId && cm.isDamageRoll && cm.item === secondary.item ) {
+        sd = cm.rolls[0];
+    }
+}
+
+Hooks.on('renderChatMessage', PD);
+Hooks.on('renderChatMessage', SD);
 
 Hooks.once('renderChatMessage', PDOS);
 await primary.attack({event});
@@ -37,57 +49,72 @@ await primary.attack({event});
 Hooks.once('renderChatMessage', SDOS);
 await secondary.attack({event});
 
-if ( pdos === "failure" || pdos === "criticalFailure" ) {
-    if ( sdos === "success" ) {
-        return await secondary.damage({event})
+if ( (!game.modules.has('xdy-pf2e-workbench') || !game.modules.get('xdy-pf2e-workbench')?.active ) || ( game.modules.get('xdy-pf2e-workbench')?.active && !game.settings.get("xdy-pf2e-workbench","autoRollDamageForStrike")) ) {
+    if ( pdos === "failure" || pdos === "criticalFailure" ) {
+        if ( sdos === "success" ) {
+            return await secondary.damage({event})
+        }
+        if ( sdos === "criticalSuccess" ) { 
+            return await secondary.critical({event})
+        }
+        else { return }
     }
-    if ( sdos === "criticalSuccess" ) { 
-        return await secondary.critical({event})
+    
+    if ( sdos === "failure" || sdos === "criticalFailure" ) {
+        if ( pdos === "success" ) {
+            return await primary.damage({event})
+        }
+        if ( pdos === "criticalSuccess" ) { 
+            return await primary.critical({event}) 
+        }
+        else { return }
     }
-    else { return }
-}
 
-if ( sdos === "failure" || sdos === "criticalFailure" ) {
     if ( pdos === "success" ) {
-        return await primary.damage({event})
+        await primary.damage({event})
     }
-    if ( pdos === "criticalSuccess" ) { 
-        return await primary.critical({event}) 
+    
+    if ( pdos === "criticalSuccess" ) {
+        await primary.critical({event})
     }
-    else { return }
+    
+    if ( sdos === "success" ) {
+        await secondary.damage({event})
+    }
+    
+    if ( sdos === "criticalSuccess" ) {
+        await secondary.critical({event})
+    }
 }
 
-if ( pdos === "success" ) {
-    pd = await primary.damage({event})
-}
+await new Promise(async (resolve) => {
+    setTimeout(resolve,200);
+});
 
-if ( pdos === "criticalSuccess" ) {
-    pd = await primary.critical({event})
-}
-
-if ( sdos === "success" ) {
-    sd = await secondary.damage({event})
-}
-
-if ( sdos === "criticalSuccess" ) {
-    sd = await secondary.critical({event})
-}
-
-const ft = `{${pd._formula.replace(/{/g, '').replace(/}/g, '')},${sd._formula.replace(/{/g, '').replace(/}/g, '')}}`;
+let ft = `{`
 const instances = pd.instances.concat(sd.instances);
-
-const dam = new DamageRoll(ft);
-dam._total = pd.total + sd.total;
-dam._evaluated = true;
 
 let n = 0;
 for ( i of instances ) {
     const x = n++;
-    dam.instances[x]._total = i.total;
-    dam.instances[x]._evaluated = true;
+    if ( x === 0 ) {
+        ft += `${i.total}[${i.options.flavor}]`
+    }
+    else {
+        if ( i.persistent ) {
+            ft += `,${i._formula}`
+        }
+        else {
+        ft += `,${i.total}[${i.options.flavor}]`
+        }
+    }
 }
+ft += `}`
 
-await dam.toMessage({
+Hooks.off('renderChatMessage', PD);
+Hooks.off('renderChatMessage', SD);
+
+await new DamageRoll(ft).toMessage({
     flavor: `<strong>Double Slice Total Damage</strong>`,
     speaker: ChatMessage.getSpeaker()
 });
