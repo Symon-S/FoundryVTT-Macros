b237e66366c607b5b723a194b1869baec0cd56be
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2023-01-15 20:27:27 +0000

    -Fixed Misspelled DamageRoll function

--- PF2e/Contributions by others/Treat Wounds and Battle Medicine.js
+++ PF2e/Contributions by others/Treat Wounds and Battle Medicine.js
@@ -233,7 +233,7 @@ const rollTreatWounds = async ({
            user: game.user.id,
            type: CONST.CHAT_MESSAGE_TYPES.ROLL,
            flavor: `<strong>Damage Roll: Risky Surgery</strong>`,
-           roll: await new DamaageRoll('1d8[slashing]').roll({ async: true }),
+           roll: await new DamageRoll('1d8[slashing]').roll({ async: true }),
            speaker: ChatMessage.getSpeaker(),
          });
        }
