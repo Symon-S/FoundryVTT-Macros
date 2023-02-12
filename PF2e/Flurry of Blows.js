77e047c7fd68cd17a5cbafb5afdc400d4051001a
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2023-02-12 09:40:40 +0000

    Update Flurry of Blows.js -Cleaned up code a bit -Tried to make Dialogs nicer

--- PF2e/Flurry of Blows.js
+++ PF2e/Flurry of Blows.js
@@ -17,7 +17,7 @@ for ( const w of weapons ) {
     wtcf += `<option value=${w.item.id}>${w.item.name}</option>`
 }
 
-const { cWeapon, bypass } = await Dialog.wait({
+const { cWeapon, map, bypass, dos} = await Dialog.wait({
     title:"Flurry of Blows",
     content: `
         <select id="fob1" autofocus>
@@ -25,18 +25,62 @@ const { cWeapon, bypass } = await Dialog.wait({
         </select><br>
         <select id="fob2">
             ${wtcf}
-        </select>
-        <hr>
-        <div>
-        <label><input type="checkbox" id="cb1" style="width: 15px;vertical-align: center">Bypass strikes? (rerolled a strike with a Hero Point)</label>
-        </div>
-        <hr>
+        </select><hr>
+            <select id="map">
+            <option value=0>No MAP</option>
+            <option value=1>MAP 1</option>
+            <option value=2>MAP 2</option>
+        </select><hr>
     `,
     buttons: {
             ok: {
                 label: "FoB",
                 icon: "<i class='fa-solid fa-hand-fist'></i>",
-                callback: (html) => { return { cWeapon: [html[0].querySelector("#fob1").value,html[0].querySelector("#fob2").value], bypass: html[0].querySelector("#cb1").checked } }
+                callback: (html) => { return { cWeapon: [html[0].querySelector("#fob1").value,html[0].querySelector("#fob2").value], map: parseInt(html[0].querySelector("#map").value), bypass: false } }
+            },
+            bypass: {
+                label:"Bypass",
+                icon: "<i class='fa-solid fa-forward'></i>",
+                callback: async (html) => {
+                    const cWeapon = [html[0].querySelector("#fob1").value,html[0].querySelector("#fob2").value];
+                    const map = parseInt(html[0].querySelector("#map").value);
+                    const dos = await Dialog.wait({
+                        title:"Degree of Success",
+                        content: `
+                            <table>
+                                <tr>
+                                    <td>${weapons.find( w => w.item.id === cWeapon[0] ).label} (1st)</td>
+                                    <td><select id="dos1" autofocus>
+                                    <option value=2>Success</option>
+                                    <option value=3>Critical Success</option>
+                                    <option value=1>Failure</option>
+                                    <option value=0>Critical Failure</option>
+                                    </select></td></tr>
+                                <tr>
+                                    <td>${weapons.find( w => w.item.id === cWeapon[1] ).label} (2nd)</td>
+                                    <td><select id="dos2">
+                                        <option value=2>Success</option>
+                                        <option value=3>Critical Success</option>
+                                        <option value=1>Failure</option>
+                                        <option value=0>Critical Failure</option>
+                                    </select></td></tr>
+                            </table><hr>
+                        `,
+                        buttons: {
+                            ok: {
+                                label: "Reroll",
+                                icon: "<i class='fa-solid fa-dice'></i>",
+                                callback: (html) => { return [parseInt(html[0].querySelector("#dos1").value), parseInt(html[0].querySelector("#dos2").value) ] }
+                            },
+                            cancel: {
+                                label: "Cancel",
+                                icon: "<i class='fa-solid fa-ban'></i>",
+                            }
+                        },
+                        default: 'ok'
+                    },{width:"auto"});
+                    return { cWeapon, map, bypass: true, dos }
+                },
             },
             cancel: {
                 label: "Cancel",
@@ -46,69 +90,13 @@ const { cWeapon, bypass } = await Dialog.wait({
     default: "ok"
 },{width:"auto"});
 
+const map2 = map === 2 ? map : map + 1;
 if ( cWeapon === undefined ) { return; }
 
 const primary = weapons.find( w => w.item.id === cWeapon[0] );
 const secondary = weapons.find( w => w.item.id === cWeapon[1] );
 
 let options = token.actor.itemTypes.feat.some(s => s.slug === "stunning-fist") ? ["stunning-fist"] : [];
-
-const map = bypass ? await Dialog.wait({
-    title:"Degree of Success",
-    content: `
-        <label>
-        <select id="dos1" autofocus>
-            <option value=2>Success</option>
-            <option value=3>Critical Success</option>
-            <option value=1>Failure</option>
-            <option value=0>Critical Failure</option>
-        </select><hr>
-        
-        <select id="dos2" autofocus>
-            <option value=2>Success</option>
-            <option value=3>Critical Success</option>
-            <option value=1>Failure</option>
-            <option value=0>Critical Failure</option>
-        </select><hr>
-    `,
-    buttons: {
-            ok: {
-                label: "Reroll",
-                icon: "<i class='fa-solid fa-dice'></i>",
-                callback: (html) => { return [parseInt(html[0].querySelector("#dos1").value), parseInt(html[0].querySelector("#dos2").value) ] }
-            },
-            cancel: {
-                label: "Cancel",
-                icon: "<i class='fa-solid fa-ban'></i>",
-            }
-    },
-    default: 'ok'
-},{width:250}) : 
-
-await Dialog.wait({
-    title:"Current MAP",
-    content: `
-        <select autofocus>
-            <option value=0>No MAP</option>
-            <option value=1>MAP 1</option>
-            <option value=2>MAP 2</option>
-        </select><hr>
-    `,
-    buttons: {
-            ok: {
-                label: "MAP",
-                icon: "<i class='fa-solid fa-plus-minus'></i>",
-                callback: (html) => { return parseInt(html[0].querySelector("select").value) }
-            },
-            cancel: {
-                label: "Cancel",
-                icon: "<i class='fa-solid fa-ban'></i>",
-            }
-    },
-    default: 'ok'
-},{width:250});
-
-if ( map === "cancel" ) { return; }
 
 const cM = [];
 function PD(cm) {
@@ -122,11 +110,10 @@ function PD(cm) {
 
 Hooks.on('preCreateChatMessage', PD);
 
-const map2 = map === 2 ? map : map + 1;
 
-const pdos = bypass ? map[0] : (await primary.variants[map].roll({skipDialog:true, event })).degreeOfSuccess;
+const pdos = bypass ? dos[0] : (await primary.variants[map].roll({skipDialog:true, event })).degreeOfSuccess;
 
-const sdos = bypass ? map[1] : (await secondary.variants[map2].roll({skipDialog:true, event})).degreeOfSuccess;
+const sdos = bypass ? dos[1] : (await secondary.variants[map2].roll({skipDialog:true, event})).degreeOfSuccess;
 
 let pd,sd;
 if ( pdos === 2 ) { pd = await primary.damage({event}); }
@@ -158,6 +145,10 @@ if ( pdos <= 1 ) {
     } 
 }
 
+await new Promise( (resolve) => {
+    setTimeout(resolve,0);
+});
+
 if ( pdos <=0 && sdos <= 1 ) { return }
 
 else {    
@@ -165,8 +156,8 @@ else {
     const type = pd.terms[0].rolls.map(t => t.type).concat(sd.terms[0].rolls.map(t => t.type));
     const persistent = pd.terms[0].rolls.map(t => t.persistent).concat(sd.terms[0].rolls.map(t => t.persistent));
     
-    let preCombinedDamage = []
-    let combinedDamage = '{'
+    let preCombinedDamage = [];
+    let combinedDamage = '{';
     let i = 0;
     for ( const t of terms ) {
         if ( persistent[i] && !preCombinedDamage.find( p => p.persistent && p.terms.includes(t) ) ) {
@@ -196,7 +187,7 @@ else {
                 }
             }
             else if ( p.terms.length === 1 ) {
-                combinedDamage += `, ${p.terms[0]}`
+                combinedDamage += `, ${p.terms[0]}`;
             }
             else {
                 combinedDamage += `, (${p.terms.join(" + ")})[${p.type}]`;
@@ -207,14 +198,15 @@ else {
     combinedDamage += "}";
     
     const rolls = [await new DamageRoll(combinedDamage).evaluate({ async: true })]
-    let flavor = `<strong>Flurry of Blows Total Damage</strong>`
-    if ( cM.length === 1 ) { flavor += `<hr>${cM[0].flavor}` }
-    else { flavor += `<hr>${cM[0].flavor}<hr>${cM[1].flavor}` }
+    let flavor = `<strong>Flurry of Blows Total Damage</strong>`;
+    const color = pdos === 2 ? `<span style="color:rgb(0, 0, 255)">Success</span>` : `<span style="color:rgb(0, 128, 0)">Critical Success</span>`
+    if ( cM.length === 1 ) { flavor += `<p>Same Weapon (${color})<hr>${cM[0].flavor}</p><hr>`; }
+    else { flavor += `<hr>${cM[0].flavor}<hr>${cM[1].flavor}`; }
     if ( options.includes("stunning-fist") ) {
-       flavor += `<br><strong>Stunning Fist</strong>    ${game.i18n.localize("PF2E.SpecificRule.Monk.StunningFist.Note")}`
+       flavor += `<br><strong>Stunning Fist</strong> ${game.i18n.localize("PF2E.SpecificRule.Monk.StunningFist.Note")}`;
     }
     if ( pdos === 3 || sdos === 3 ) {
-        flavor += `<hr><strong>TOP DAMAGE USED FOR CREATURES IMMUNE TO CRITICALS`
+        flavor += `<hr><strong>TOP DAMAGE USED FOR CREATURES IMMUNE TO CRITICALS`;
         if ( critRule === "doubledamage" ) {
             rolls.unshift(await new DamageRoll(combinedDamage.replaceAll("2 * ", "")).evaluate({ async: true }));
         }
@@ -232,17 +224,17 @@ else {
                         const n = sV.split(/(d\d)/g);
                         if ( n[0].charAt(1) !== "(") {
                             n[0] = `${parseInt(n[0].charAt(1) / 2)}`;
-                            toJoinVAlues.push(n.join(""))
+                            toJoinVAlues.push(n.join(""));
                             continue;
                         }
                         else if ( n[0].charAt(2) !== "(") { 
-                            n[0] = `(${parseInt(n[0].charAt(2)) / 2}`
-                            toJoinVAlues.push(n.join(""))
+                            n[0] = `(${parseInt(n[0].charAt(2)) / 2}`;
+                            toJoinVAlues.push(n.join(""));
                             continue;
                         }
                         else { 
-                            n[0] = `((${parseInt(n[0].charAt(3)) / 2}`
-                            toJoinVAlues.push(n.join(""))
+                            n[0] = `((${parseInt(n[0].charAt(3)) / 2}`;
+                            toJoinVAlues.push(n.join(""));
                             continue;
                         }
                     }
