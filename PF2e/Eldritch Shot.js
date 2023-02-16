7d5356db5dda49badad9b02b683b6928f6b11019
Symon S <8838924-symonsch@users.noreply.gitlab.com>
2023-02-16 01:36:40 +0000

    Update Eldritch Shot.js -Fixed for acid splash below player level 5, spell level 3

--- PF2e/Eldritch Shot.js
+++ PF2e/Eldritch Shot.js
@@ -160,7 +160,7 @@ async function Eldritch_shot()
       /* Acid Splash */
       if(spc.slug === 'acid-splash') {
         let pers = 0;
-        spc.roll = (await (await spc.spell.loadVariant({castLevel:Math.ceil(actor.level / 2)})).getDamage()).template.damage.roll;
+        spc.roll = await spc.spell.loadVariant({castLevel:Math.ceil(actor.level / 2)}) === null ? spc.roll : (await spc.spell.loadVariant({castLevel:Math.ceil(actor.level / 2)}).getDamage()).template.damage.roll;
         if (actor.level < 5) {
           pers = 1;
           splash = '1'
