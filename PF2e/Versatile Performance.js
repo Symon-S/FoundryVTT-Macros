9b0b4f383bc4dac90288c369e16997424e74f03a
Symon S. <8838924-symonsch@users.noreply.gitlab.com>
2023-11-05 02:23:06 +0000

    Add new file

---
+++ PF2e/Versatile Performance.js
@@ -0,0 +1,28 @@
+const options = [
+    {name: "Demoralize", value: "demoralize"},
+    {name: "Make an Impression", value: "makeAnImpression"},
+    {name: "Impersonate", value: "impersonate"}
+];
+
+let myContent = `
+        <div class="form-group">
+          <label for="exampleSelect">Action being used</label>
+          <select id="exampleSelect">`
+
+for (i = 0; i < options.length; i++){
+  myContent += `
+                <option value="${options[i].value}">${options[i].name}</option>`
+};
+
+myContent += `
+          </select>
+        </div>`
+
+const select = await Dialog.prompt({
+    title: 'Versatile Performance',
+    content: myContent,
+        callback: async(html) => {
+          return html.find('#exampleSelect').val();
+    }
+});
+game.pf2e.actions[select]({skill:"performance"});
