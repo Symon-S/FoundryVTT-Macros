5ca6e63ce319170c67d4d71004f43b1793e4a995
Symon S. <8838924-symonsch@users.noreply.gitlab.com>
2023-11-17 10:24:41 +0000

    Update Spellsling.js

--- PF2e/Spellsling.js
+++ PF2e/Spellsling.js
@@ -25,7 +25,7 @@ async function Spellsling() {
       for (const active of level.active) {
         const index = i++;
         if (active === null) { continue; }
-        let spell = active.spell;
+        const spell = active.spell;
         if (!spell.system.traits.value.includes("attack") || spell === null) { continue; }
         const castLevel = active.castLevel ?? (await spell.getChatData()).castLevel;
         const { isAttack, isSave, description, save, slug, traits, formula } = await spell.getChatData({}, { castLevel });
