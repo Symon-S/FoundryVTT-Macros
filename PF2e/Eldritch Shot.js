1d5078bda9469cf36d0c3f3a892c06e69f727df6
Symon S. <8838924-symonsch@users.noreply.gitlab.com>
2023-11-19 02:15:58 +0000

    Update Eldritch Shot.js

--- PF2e/Eldritch Shot.js
+++ PF2e/Eldritch Shot.js
@@ -119,7 +119,7 @@ async function Eldritch_shot()
           
     // Build dialog data
     const ovr_data = [
-      { label : `Choose a Spell Variant:`, type : `select`, options : spell_variants.map(p => p.name) }
+      { label : `Choose a Spell Variant : `, type : `select`, options : spell_variants.map(p => p.name) }
     ];
                  
     // Query user for variant choice
