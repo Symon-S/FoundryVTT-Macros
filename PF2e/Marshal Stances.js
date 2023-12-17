f9feb311e4d7d920491b18662771bf3189e6228b
Symon S. <8838924-symonsch@users.noreply.gitlab.com>
2023-12-17 01:42:34 +0000

    Update Marshal Stances.js -Updated code a little & added Devrin's Cunning Stance

--- PF2e/Marshal Stances.js
+++ PF2e/Marshal Stances.js
@@ -6,23 +6,35 @@ Does not work without workbench module.
 if (!game.modules.get('xdy-pf2e-workbench')?.active) { return ui.notifications.error('This macro requires workbench module to be installed!'); }
 if (!actor || token.actor.type !== 'character') { return ui.notifications.warn("You must have a PC token selected"); }
 if (canvas.tokens.controlled.length > 1) { return ui.notifications.warn("Please select the token with the Marshal Dedication"); }
-if (!token.actor.itemTypes.feat.some(ms => ms.slug === "dread-marshal-stance" || ms.slug === "inspiring-marshal-stance")) { return ui.notifications.warn("The actor does not possess a Marshal Stance feat"); }
+if (!token.actor.itemTypes.feat.some(ms => ms.slug === "dread-marshal-stance" || ms.slug === "inspiring-marshal-stance" || ms.slug === "devrins-cunning-stance")) { return ui.notifications.warn("The actor does not possess a Marshal Stance feat"); }
 const modifiers = [];
 const level = token.actor.level;
 let skillName,skillKey,actionSlug,actionName,choice,notes;
-if (token.actor.itemTypes.feat.some(ms => ms.slug === "dread-marshal-stance") && token.actor.itemTypes.feat.some(ms => ms.slug === "inspiring-marshal-stance")) {
-  choice = await new Promise((resolve) => {
-    new Dialog({
-      title: 'Which Stance?',
-      buttons: {
-        ims: { label: 'Inspiring Marshal Stance', callback: async() => { resolve("ims"); } },
-        dms: { label: 'Dread Marshal Stance', callback: async() => { resolve("dms"); } },
-      }
-    }).render(true);
-  });
+let buttons = {};
+if (token.actor.itemTypes.feat.some(ms => ms.slug === "inspiring-marshal-stance")) { 
+  buttons.ims = { label: 'Inspiring Marshal Stance' };
 }
-else if (token.actor.itemTypes.feat.some(ms => ms.slug === "dread-marshal-stance") && token.actor.itemTypes.feat.some(ms => ms.slug !== "inspiring-marshal-stance")) { choice = "dms"; }
-else if (token.actor.itemTypes.feat.some(ms => ms.slug !== "dread-marshal-stance") && token.actor.itemTypes.feat.some(ms => ms.slug === "inspiring-marshal-stance")) { choice = "ims"; }
+if (token.actor.itemTypes.feat.some(ms => ms.slug === "dread-marshal-stance")) {
+  buttons.dms = { label: 'Dread Marshal Stance' };
+}
+if (token.actor.itemTypes.feat.some(ms => ms.slug === "devrins-cunning-stance")) { 
+  buttons.dcs = { label: 'Devrins Cunning Stance' };
+}
+
+if (Object.keys(buttons).length > 1) {
+  let width = 400;
+  if (Object.keys(buttons).length > 2 ) { width = 600 }
+  choice = await Dialog.wait({
+    title: 'Which Stance?',
+    buttons,
+    close: () => { return "close" }
+  },{width});
+}
+else {
+  choice = Object.keys(buttons)[0];
+}
+
+if ( choice === "close" ) { return ui.notifications.warn("You did not select a stance"); }
 
 let img = 'systems/pf2e/icons/features/feats/dread-marshal-stance.webp'
 let stance = 'dmscf';
@@ -36,13 +48,12 @@ if (choice === "ims") {
   actionSlug = "inspiring-marshal-stance";
   actionName = "Inspiring Marshal Stance";
   notes.push({"outcome":["success"], "selector":"diplomacy", "text":`<p>Your @Compendium[pf2e.feat-effects.kzEPq4aczYb6OD2h]{Marshal's Aura} grants you and allies a +1 status bonus to attack rolls and saves against mental effects.</p>`});
-    notes.push({"outcome":["criticalSuccess"], "selector":"diplomacy", "text":`<p>Your @Compendium[pf2e.feat-effects.kzEPq4aczYb6OD2h]{Marshal's Aura} increases to 20 feet and grants you and allies a +1 status bonus to attack rolls and saves against mental effects.</p>`});
-    notes.push({"outcome":["failure"], "selector":"diplomacy", "text":`<p>You fail to enter the stance.</p>`});
-notes.push({"outcome":["criticalFailure"], "selector":"diplomacy", "text":`<p>You fail to enter the stance and can't take this action again for 1 minute.</p>`});
+  notes.push({"outcome":["criticalSuccess"], "selector":"diplomacy", "text":`<p>Your @Compendium[pf2e.feat-effects.kzEPq4aczYb6OD2h]{Marshal's Aura} increases to 20 feet and grants you and allies a +1 status bonus to attack rolls and saves against mental effects.</p>`});
+  notes.push({"outcome":["failure"], "selector":"diplomacy", "text":`<p>You fail to enter the stance.</p>`});
+  notes.push({"outcome":["criticalFailure"], "selector":"diplomacy", "text":`<p>You fail to enter the stance and can't take this action again for 1 minute.</p>`});
 }
-
 if (choice === "dms") {
-    if (token.actor.itemTypes.effect.some(e => e.slug === 'dmscf')) { return ui.notifications.warn('You cannot enter a marshal stance while in cooldown!'); }
+  if (token.actor.itemTypes.effect.some(e => e.slug === 'dmscf')) { return ui.notifications.warn('You cannot enter a marshal stance while in cooldown!'); }
   skillName = "Intimidation";
   skillKey = "intimidation"
   notes = [];
@@ -51,7 +62,21 @@ if (choice === "dms") {
   notes.push({"outcome":["success"], "selector":"intimidation", "text":`<p>Your @Compendium[pf2e.feat-effects.KBEJVRrie2JTHWIK]{Marshal's Aura} grants you and allies a status bonus to damage rolls equal to the number of weapon damage dice of the unarmed attack or weapon you are wielding that has the most weapon damage dice. When you or an ally in the aura critically hits an enemy with a Strike, that enemy is @Compendium[pf2e.conditionitems.TBSHQspnbcqxsmjL]{Frightened 1}.</p>`});
   notes.push({"outcome":["criticalSuccess"], "selector":"intimidation", "text":`<p>Your @Compendium[pf2e.feat-effects.KBEJVRrie2JTHWIK]{Marshal's Aura} increases to 20 feet, and it grants you and allies a status bonus to damage rolls equal to the number of weapon damage dice of the unarmed attack or weapon you are wielding that has the most weapon damage dice. When you or an ally in the aura critically hits an enemy with a Strike, that enemy is @Compendium[pf2e.conditionitems.TBSHQspnbcqxsmjL]{Frightened 1}.</p>`});
   notes.push({"outcome":["failure"], "selector":"intimidation", "text":`<p>You fail to enter the stance.</p>`});
-notes.push({"outcome":["criticalFailure"], "selector":"intimidation", "text":`<p>You fail to enter the stance and can't take this action again for 1 minute.</p>`});
+  notes.push({"outcome":["criticalFailure"], "selector":"intimidation", "text":`<p>You fail to enter the stance and can't take this action again for 1 minute.</p>`});
+}
+if (choice === "dcs") {
+  if (token.actor.itemTypes.effect.some(e => e.slug === 'dcscf')) { return ui.notifications.warn('You cannot enter a marshal stance while in cooldown!'); }
+  stance = "dcscf"
+  img = "systems/pf2e/icons/spells/anticipate-peril.webp"
+  skillName = "Deception";
+  skillKey = "deception"
+  notes = [];
+  actionSlug = "devrins-cunning-stance";
+  actionName = "Devrin's Cunning Stance";
+  notes.push({"outcome":["success"], "selector":"deception", "text":`<p>Your @Compendium[pf2e.feat-effects.jACKRmVfr9ATsmwg]{Marshal's Aura} grants you and your allies a +1 status bonus to skill checks. When you or an ally in the aura Strike an @UUID[Compendium.pf2e.conditionitems.Item.AJh5ex99aV6VTggg] enemy, that enemy can’t use reactions until the beginning of its next turn.</p>`});
+  notes.push({"outcome":["criticalSuccess"], "selector":"deception", "text":`<p>Your @Compendium[pf2e.feat-effects.jACKRmVfr9ATsmwg]{Marshal's Aura} increases to a 20-foot emanation, and it grants you and your allies a +1 status bonus to skill checks. When you or an ally in the aura Strike an @UUID[Compendium.pf2e.conditionitems.Item.AJh5ex99aV6VTggg] enemy, that enemy can’t use reactions until the beginning of its next turn.</p>`});
+  notes.push({"outcome":["failure"], "selector":"deception", "text":`<p>You fail to enter the stance.</p>`});
+  notes.push({"outcome":["criticalFailure"], "selector":"deception", "text":`<p>You fail to enter the stance and can't take this action again for 1 minute.</p>`});
 }
 
 const custom = await new Promise((resolve) => {
@@ -61,7 +86,8 @@ const custom = await new Promise((resolve) => {
       yes: { label: 'Yes', callback: async() => { resolve(true); } },
       no: { label: 'No', callback: async() => { resolve(false); } },
     },
-    default: 'no'
+    default: 'no',
+    close: () => { return; }
   }).render(true);
 });
 
@@ -75,12 +101,13 @@ options.push(`action:${actionSlug}`);
 if (custom) { await new Promise(() => {
     new Dialog({
       title: 'Enter a custom DC',
-      content: `DC: <input id="dcinput" type="number" autofocus style="width: 50px;" value=''>`,
+      content: `DC:&nbsp <input id="dcinput" type="number" autofocus style="width: 25px;text-align: center;" value=''>`,
       buttons: {
         enter: { label: 'Enter', callback: (html) => { main(html); } },
         cancel: { label: 'Cancel', callback: () => { return; } },
       },
-      default: 'enter'
+      default: 'enter',
+      close: () => { return; }
     }).render(true);
   });
 }
@@ -103,41 +130,34 @@ async function SRoll() {
 	null,
   );
 
-  if (roll.options.degreeOfSuccess === 2) {
+  if (roll.options.degreeOfSuccess > 1) {
     if(choice === 'dms'){
       const effect = (await fromUuid('Compendium.pf2e.feat-effects.qX62wJzDYtNxDbFv')).toObject();
       effect.img = img;
       effect.system.rules.shift();
       effect.system.rules[0].radius = 10;
+      if (roll.options.degreeOfSuccess === 3) { effect.system.rules[0].radius = 20; }
       effect.system.rules[0].slug = "marshal-drd-stance";
       await token.actor.createEmbeddedDocuments("Item", [effect]);
     }
-    if (choice === 'ims'){
-      const effect = (await fromUuid("Compendium.pf2e.feat-effects.er5tvDNvpbcnlbHQ")).toObject();
-      effect.img = img;
-      effect.system.rules.shift();
-      effect.system.rules[0].radius = 10;
-      effect.system.rules[0].slug = "marshal-insp-stance";
-      await token.actor.createEmbeddedDocuments("Item", [effect]);
-    }
-  }
-  if (roll.options.degreeOfSuccess === 3) {
-    if(choice === 'dms'){
-      const effect = (await fromUuid('Compendium.pf2e.feat-effects.qX62wJzDYtNxDbFv')).toObject();
-      effect.img = img;
-      effect.system.rules.shift();
-      effect.system.rules[0].radius = 20;
-      effect.system.rules[0].slug = "marshal-drd-stance-cs";
-      await token.actor.createEmbeddedDocuments("Item", [effect]);
-    }
-    if (choice === 'ims'){
-      const effect = (await fromUuid('Compendium.pf2e.feat-effects.er5tvDNvpbcnlbHQ')).toObject();
-      effect.img = img;
-      effect.system.rules.shift();
-      effect.system.rules[0].radius = 20;
-      effect.system.rules[0].slug = "marshal-insp-stance-cs";
-      await token.actor.createEmbeddedDocuments("Item", [effect]);
-    }
+      if (choice === 'ims'){
+        const effect = (await fromUuid("Compendium.pf2e.feat-effects.er5tvDNvpbcnlbHQ")).toObject();
+        effect.img = img;
+        effect.system.rules.shift();
+        effect.system.rules[0].radius = 10;
+        if (roll.options.degreeOfSuccess === 3) { effect.system.rules[0].radius = 20; }
+        effect.system.rules[0].slug = "marshal-insp-stance";
+        await token.actor.createEmbeddedDocuments("Item", [effect]);
+      }
+      if(choice === 'dcs'){
+        const effect = (await fromUuid('Compendium.pf2e.feat-effects.Item.kyrvZfZfzKK1vx9b')).toObject();
+        effect.img = img;
+        effect.system.rules.shift();
+        effect.system.rules[0].radius = 10;
+        if (roll.options.degreeOfSuccess === 3) { effect.system.rules[0].radius = 20; }
+        effect.system.rules[0].slug = "marshal-dcs-stance";
+        await token.actor.createEmbeddedDocuments("Item", [effect]);
+      }
   }
   if (roll.options.degreeOfSuccess === 0) {
     const effect = {     
