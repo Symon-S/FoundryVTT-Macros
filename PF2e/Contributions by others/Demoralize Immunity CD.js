caa3beeeb1943f619c932872c55586646a77b92c
kromko <igor.romanishkin@gmail.com>
2024-01-22 23:24:21 +0000

    Demoralize: Add support for trait immunities, output improvements

--- PF2e/Contributions by others/Demoralize Immunity CD.js
+++ PF2e/Contributions by others/Demoralize Immunity CD.js
@@ -1,6 +1,7 @@
 /*
 Based on the macro Contributed by ArthurTrumpet
 modified by darkim
+modified again by kromko
 
 Uses the a new effect to track who has demoralized whom.
 Immunity duration is set depending on several factors. Default is 10 minutes. 
@@ -9,77 +10,59 @@ To remove an effect, right click it in the effect tracker (top right of the scre
 */
 
 const demEffect = {
-    type: 'effect',
-    name: 'Demoralize Immunity',
-    img: 'systems/pf2e/icons/spells/blind-ambition.webp',
-    system: {
-      slug: 'demoralize-immunity',
-      tokenIcon: {
-          show: true
-      },       
-      duration: {
-          value: 10,
-          unit: 'minutes',
-          sustained: false,
-          expiry: 'turn-end'
-      },
-      rules: [],
+  type: 'effect',
+  name: 'Demoralize Immunity',
+  img: 'systems/pf2e/icons/spells/blind-ambition.webp',
+  system: {
+    slug: 'demoralize-immunity',
+    tokenIcon: {
+        show: true
+    },       
+    duration: {
+        value: 10,
+        unit: 'minutes',
+        sustained: false,
+        expiry: 'turn-end'
     },
-  };
-/*
-const retreatEffect = {
-    type: 'effect',
-    name: 'Terrified Retreat',
-    slug: 'terrified-retreat-effect',
-    img: 'systems/pf2e/icons/conditions/fleeing.webp',
-    system: {
-      tokenIcon: {
-          show: true
-      },       
-      duration: {
-          value: 1,
-          unit: 'rounds',
-          sustained: false,
-          expiry: 'turn-end'
-      },
-      rules: [
-        {
-          "key": "GrantItem",
-          "uuid": "Compendium.pf2e.conditionitems.Item.sDPxOjQ9kx2RZE8D",
-          "onDeleteActions": {
-            "grantee": "restrict"
-          }
-        }
-      ],
-    },
-  };
-*/
+    rules: [],
+  },
+};
+
 main();
 
 
 async function main() {
-    const message = game.messages.contents.reverse().find( m => m.flags.demoralize);
+  const message = game.messages.contents.findLast( m => m.flags.demoralize);
 
-    let {sourceTokenId, sourceName, targetTokenId, targetName, degreeOfSuccess} = message.flags.demoralize;
+  let {sourceTokenId, sourceName, results, terrifiedRetreat} = message.flags.demoralize;
 
-    let source = canvas.tokens.get(sourceTokenId);
+  let source = canvas.tokens.get(sourceTokenId);
+  
+  results = results.filter(r=>r.outcome !== 'invalid');
+  // Check if we have permissions to edit the target actor
+  if(!results.every(r=>canvas.tokens.get(r.targetTokenId)?.actor.isOwner))
+      return ui.notification.warn("You don't have permissions to edit some of the tokens");
+
+  demEffect.name = `Demoralize Immunity from ${sourceName}`;
+  demEffect.img = source.actor.prototypeToken.texture.src;
+  demEffect.flags = {demoralize: {source: source.actor.id}};
+
+  for(let {targetTokenId, outcome} of results){
     let target = canvas.tokens.get(targetTokenId);
-    
-    // Check if we have permissions to edit the target actor
-    if(!target.actor.isOwner)
-        return ui.notification.warn("You don't have permissions to edit the target token");
 
-    demEffect.name = `Demoralize Immunity from ${sourceName}`;
-    demEffect.img = source.actor.prototypeToken.texture.src;
-    demEffect.flags = {demoralize: {source: source.actor.id}};
-
-    if(['success', 'criticalSuccess'].includes(degreeOfSuccess)){
-        let frightened = target.actor.getCondition('frightened')?.value ?? 0;
-        let newFrightened = Math.max(frightened, degreeOfSuccess == 'success' ? 1 : 2);
-        target.actor.increaseCondition('frightened', {value:newFrightened, max:newFrightened});
+    // Frightened
+    if(['success', 'criticalSuccess'].includes(outcome)){
+      let frightened = target.actor.getCondition('frightened')?.value ?? 0;
+      let newFrightened = Math.max(frightened, outcome == 'success' ? 1 : 2);
+      target.actor.increaseCondition('frightened', {value:newFrightened, max:newFrightened});
     }
+
+    // Terrified retreat
+    if(terrifiedRetreat && outcome === 'criticalSuccess'){
+      target.actor.increaseCondition('fleeing', {max:1});
+    }
+
+    // Temporary immunity
     await target.actor.createEmbeddedDocuments("Item", [demEffect]);
-
-    ui.notifications.info(`${targetName} is now immune to Demoralize by ${sourceName}`);
+  }
 }
-
