69f6f25af49bd7a61ad5ccd502e325084afbfb33
Symon S. <8838924-symonsch@users.noreply.gitlab.com>
2024-05-08 01:18:23 +0000

    Update Heroic Recovery.js - Fixed forgotten return

--- PF2e/Heroic Recovery.js
+++ PF2e/Heroic Recovery.js
@@ -4,7 +4,7 @@ https://2e.aonprd.com/Rules.aspx?ID=382
 It does not set hitpoints to 0, since if you are dying your hitpoints should already be 0
 */
 
-if ( !actor || !actor.system.resources?.heroPoints?.value ) { ui.notifications.warn("You have not selected a Player character's token"); }
+if ( !actor || !actor.system.resources?.heroPoints?.value ) { return void ui.notifications.warn("You have not selected a Player character's token"); }
 if ( actor.system.resources.heroPoints.value < 1 ) { return void ui.notifications.info(`${token.name} is out of hero points`) }
 if ( !actor.hasCondition("dying") ) { return void ui.notifications.info(`${token.name} is not dying`) }
 await actor.update({"system.resources.heroPoints.value": 0});
