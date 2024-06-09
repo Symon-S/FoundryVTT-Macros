b3d85dcb5527670d81206c6c0ed4390e929f7882
Symon S. <8838924-symonsch@users.noreply.gitlab.com>
2024-06-09 05:25:59 +0000

    Update Hunt Shared Triple Prey.js - Added link to Hunter's Edge effects when sharing prey

--- PF2e/Hunt Shared Triple Prey.js
+++ PF2e/Hunt Shared Triple Prey.js
@@ -1,7 +1,7 @@
 /* 
 Hunt Prey/Double Prey/Shared Prey/Triple Threat.
 This macro covers all the above listed features/feats.
-Simply target the appropriate amount of targets and fill out the Dialog if you have Shared Prey or Triple Threat.
+Simply target the appropriate amount of targets and fill out the Dialog if you have Triple Threat.
 */
 
 if (!actor.items.some(hp => hp.slug === "hunt-prey")) return void ui.notifications.warn(`${token.name} does not have the hunt prey action`);
@@ -104,7 +104,30 @@ else {
   await actor.createEmbeddedDocuments("Item", updates);
 }
 
+const uuids = [
+  "@UUID[Compendium.pf2e.feat-effects.Item.uXCU8GgriUjuj5FV]", //flurry effect
+  "@UUID[Compendium.pf2e.feat-effects.Item.4UNQfMrwfWirdwoV]", //masterful flurry effect
+  "@UUID[Compendium.pf2e.feat-effects.Item.ltIvO9ZQlmqGD89Y]", //outwit effect
+  "@UUID[Compendium.pf2e.feat-effects.Item.iqvurepX0zyu9OlI]", //masterful outwit efect
+  "@UUID[Compendium.pf2e.feat-effects.Item.mNk0KxsZMFnDjUA0]", //precision effect
+  "@UUID[Compendium.pf2e.feat-effects.Item.Lt5iSfx8fxHSdYXz]", //masterful precision effect
+]; //add additional uuid's to effects of custom Hunter's Edge
+let uuid = "";
+if (actor.items.some(s => s.sourceId === "Compendium.pf2e.classfeatures.Item.6v4Rj7wWfOH1882r")) {
+  uuid = uuids[0];
+  if (actor.level > 16) { uuid = uuids[1] }
+} //flurry
+if (actor.items.some(s => s.sourceId === "Compendium.pf2e.classfeatures.Item.NBHyoTrI8q62uDsU")) {
+  uuid = uuids[2];
+  if (actor.level > 16) { uuid = uuids[3] }
+} //outwit
+if (actor.items.some(s => s.sourceId === "Compendium.pf2e.classfeatures.Item.u6cBjqz2fiRBadBt")) {
+  uuid = uuids[4];
+  if (actor.level > 16) { uuid = uuids[5] }
+} //precision
+if (uuid === "") { ui.notifications.warn("You may have a custom Hunter's Edge. If so, please modify the macro to accomodate your custom Hunter's Edge custom effect, if it has one")} // remove this line if your custom doesn't have an effect
+
 await ChatMessage.create({
   speaker: ChatMessage.getSpeaker(),
-  content: `${sp ? effect.name + "<br>" + effect2.name : effect.name}` 
+  content: `${sp ? effect.name + "<br>" + effect2.name + "<br>" + uuid : effect.name}` 
 });
