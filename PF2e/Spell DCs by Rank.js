d2fd838c71484376f175044670dacf84de45ba64
Freeze <bdevries2011@gmail.com>
2025-08-14 07:18:17 +0000

    Appv2-ified Spell DCs by Rank

--- PF2e/Spell DCs by Rank.js
+++ PF2e/Spell DCs by Rank.js
@@ -2,37 +2,24 @@
 These DCs are used for counteract checks, learning a spell, etc.
 These are NOT DCs for saves against these spells.*/
 
-const rarity = await new Promise((resolve) => {
- new Dialog({
- title: 'Rarity?',
- buttons: {
-  com: { label: 'Common', callback: async(rar) => { resolve(0); } },
-  unc: { label: 'Uncommon', callback: async(rar) => { resolve(2); } },
-  rar: { label: 'Rare', callback: async(rar) => { resolve(5); } },
-  uni: { label: 'Unique', callback: async(rar) => { resolve(10); } },
-  },
- default: 'com',
- }).render(true);
-});
+const rarityMap = new Map([["common", 0],["uncommon", 2],["rare",5],["unique",10]]);
+const sDCs = new Map([[1,15],[2,18],[3,20],[4,23],[5,26],[6,28],[7,31],[8,34],[9,36],[10,39]]);
+const optionsRank = Array.fromRange(10,1).map(e => ({label: e, value:e}))
 
-const level = await new Promise((resolve) => {
- new Dialog({
- title: 'Rank?',
- buttons: {
-  1: { label: '1', callback: async(rar) => { resolve(0); } },
-  2: { label: '2', callback: async(rar) => { resolve(1); } },
-  3: { label: '3', callback: async(rar) => { resolve(2); } },
-  4: { label: '4', callback: async(rar) => { resolve(3); } },
-  5: { label: '5', callback: async(rar) => { resolve(4); } },
-  6: { label: '6', callback: async(rar) => { resolve(5); } },
-  7: { label: '7', callback: async(rar) => { resolve(6); } },
-  8: { label: '8', callback: async(rar) => { resolve(7); } },
-  9: { label: '9', callback: async(rar) => { resolve(8); } },
-  10: { label: '10', callback: async(rar) => { resolve(9); } },
-  },
- }).render(true);
+const rarityField = new foundry.data.fields.StringField({
+  label: "PF2E.Rarity",
+  choices: CONFIG.PF2E.rarityTraits,
+  required: true
+}).toFormGroup({localize: true}, {name: "rarity", localize: true}).outerHTML;
+const rankField = new foundry.data.fields.NumberField({
+  label: "PF2E.SpellLevelLabel"
+}).toFormGroup({localize: true}, {name: "rank", options: optionsRank}).outerHTML;
+const data = await foundry.applications.api.Dialog.input({
+  window: {title: "Spell DC by Level"},
+  content:rarityField + rankField
 });
+if(!data) return;
 
-const sDCs = [15,18,20,23,26,28,31,34,36,39];
-const bDC = sDCs[level] + rarity;
+const {rank, rarity} = data;
+const bDC = sDCs.get(rank) + rarityMap.get(rarity);
 ui.notifications.info(`The Spell DC is ${bDC}`);
